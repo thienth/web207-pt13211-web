06aa6b6dccb82f96d7492df71394726cb60eb1d7
thienth32 <thienth32@gmail.com>
2019-03-13 15:22:56 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-web207',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pt13211-web';
+  name = "Thienth";
+  gender = 2;
+  classes = [
+    {
+      id: 1,
+      name: 'pt13211-web',
+      subj: "frontend framework",
+    },
+    {
+      id: 2,
+      name: 'pt13353-mob',
+      subj: "server cho android",
+    },
+    {
+      id: 3,
+      name: 'pt13354-mob',
+      subj: "server cho android",
+    }
+  ];
 }
